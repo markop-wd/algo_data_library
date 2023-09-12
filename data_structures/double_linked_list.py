06f3a3edf4354ffdabe0ca69126253e711c6032c
Marko Pomorišac <markop@mailbox.org>
2023-09-12 11:42:53 +0200

    added get

--- data_structures/double_linked_list.py
+++ data_structures/double_linked_list.py
@@ -71,3 +71,16 @@ class DoubleLinkedList:
 
         self.length -= 1
         return temp
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        if index < self.length/2:
+            for _ in range(index):
+                temp = temp.next
+        else:
+            temp = self.tail
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.previous
+        return temp
