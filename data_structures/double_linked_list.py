4a9732c24bd0945999508e6a59677b8f2c28b104
Marko Pomorišac <markop@mailbox.org>
2023-09-12 11:36:06 +0200

    added pop first

--- data_structures/double_linked_list.py
+++ data_structures/double_linked_list.py
@@ -56,3 +56,18 @@ class DoubleLinkedList:
 
         self.length += 1
         return True
+
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.previous = None
+            temp.next = None
+
+        self.length -= 1
+        return temp
