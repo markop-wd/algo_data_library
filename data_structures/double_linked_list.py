0986afd049ca8d8305a17b521f58ab1f047c101e
Marko Pomorišac <markop@mailbox.org>
2023-09-12 11:49:51 +0200

    added insert

--- data_structures/double_linked_list.py
+++ data_structures/double_linked_list.py
@@ -91,3 +91,22 @@ class DoubleLinkedList:
             temp.value = value
             return True
         return False
+
+    def insert(self, index, value):
+        if index < 0 or index >= self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+
+        new_node = Node(value)
+        before = self.get(index)
+        after = before.next
+
+        new_node.previous = before
+        new_node.next = after
+        before.next = new_node
+        after.previous = new_node
+        self.length += 1
+        return True
